bf526be3d6550bdf5302b0233a4aa1f9068fe75b
Erik <e.naaru@hotmail.com>
2019-11-20 11:26:29 +0100

    Add Lab1, Kotlin/XML version

---
+++ Lab1_Kotlin_XML/app/src/main/java/com/example/lab1_kotlin_xml/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lab1_kotlin_xml
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
